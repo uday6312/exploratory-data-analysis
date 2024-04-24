374dc855aaec31917e8d0b8461c98229bf31ea48
uday6312 <160161082+uday6312@users.noreply.github.com>
2024-04-24 14:11:05 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,5 @@
 # Coursera Data Science: Exploratory Data Analysis
 # Course project 1: plot2
-# Cheng-Han Yu
 ################################################################################
 ## load the data
 rm(list = ls())
